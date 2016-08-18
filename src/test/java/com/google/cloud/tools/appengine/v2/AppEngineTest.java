0d53a1276728844ff94d64aeb98e2ec3cf7b106b
Appu Goundan <appu@google.com>
2016-08-18 15:54:33 -0400

    Unit tests for v2

---
+++ src/test/java/com/google/cloud/tools/appengine/v2/AppEngineTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.appengine.v2;
+
+import com.google.cloud.tools.appengine.v2.internal.cloudsdk.CloudSdkAppEngineRequestFactory;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class AppEngineTest {
+
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
+  @Rule
+  public TemporaryFolder testDirectory = new TemporaryFolder();
+
+  private Path sdkHome;
+  private Path credentialFile;
+
+  @Before
+  public void setup() throws IOException {
+    sdkHome = testDirectory.newFolder("sdk").toPath();
+    credentialFile = testDirectory.newFile("crendential").toPath();
+  }
+
+  @Test
+  public void testNewRequestFactory_noConfig() {
+    exception.expect(IllegalStateException.class);
+    exception.expectMessage("No App Engine request factory implementation defined");
+
+    AppEngine.newRequestFactory().build();
+  }
+
+  @Test
+  public void testNewRequestFactory_cloudSdk() {
+    AppEngineRequestFactory rf = AppEngine.newRequestFactory().cloudSdk(sdkHome).build();
+    assertThat(rf, instanceOf(CloudSdkAppEngineRequestFactory.class));
+  }
+
+}
