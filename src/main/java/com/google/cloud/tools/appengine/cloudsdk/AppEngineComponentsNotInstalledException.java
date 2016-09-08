7d5e3a2ea959c92ed58c58cc6a255f2a3dba0645
Elliotte Rusty Harold <elharo@google.com>
2016-09-08 12:23:49 -0400

    add exception javadoc

--- src/main/java/com/google/cloud/tools/appengine/cloudsdk/AppEngineComponentsNotInstalledException.java
+++ src/main/java/com/google/cloud/tools/appengine/cloudsdk/AppEngineComponentsNotInstalledException.java
@@ -2,6 +2,9 @@ package com.google.cloud.tools.appengine.cloudsdk;
 
 import com.google.cloud.tools.appengine.api.AppEngineException;
 
+/**
+ * User needs to run <samp>gcloud components install app-engine-java</samp>.
+ */
 public class AppEngineComponentsNotInstalledException extends AppEngineException {
 
   AppEngineComponentsNotInstalledException(String message) {
