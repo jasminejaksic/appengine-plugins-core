fa9c3e4a45e0632896e078b44e02064eb194bc6b
Appu Goundan <appu@google.com>
2016-09-29 12:51:43 -0400

    Make the test more robust

--- src/test/java/com/google/cloud/tools/appengine/experimental/internal/process/CliProcessManagerTest.java
+++ src/test/java/com/google/cloud/tools/appengine/experimental/internal/process/CliProcessManagerTest.java
@@ -14,14 +14,13 @@
  * limitations under the License.
  */
 
-package com.google.cloud.tools.appengine.experimental.process;
+package com.google.cloud.tools.appengine.experimental.internal.process;
 
 import static org.junit.Assume.assumeTrue;
 
 import com.google.cloud.tools.appengine.experimental.OutputHandler;
-import com.google.cloud.tools.appengine.experimental.internal.process.CliProcessManager;
-import com.google.cloud.tools.appengine.experimental.process.io.CollectingOutputHandler;
-import com.google.cloud.tools.appengine.experimental.process.io.DumbConverter;
+import com.google.cloud.tools.appengine.experimental.internal.process.io.CollectingOutputHandler;
+import com.google.cloud.tools.appengine.experimental.internal.process.io.DumbConverter;
 
 import org.junit.Assert;
 import org.junit.Rule;
@@ -55,7 +54,6 @@ public class CliProcessManagerTest {
     Future<String> future = createTestProcess("echo 'stdout'; echo 'stderr' 1>&2", outputHandler);
 
     String result = future.get();
-    Thread.sleep(1000); // wait for stderr is complete
 
     Assert.assertEquals("stdout\n", result);
     Assert.assertEquals(Collections.singletonList("stderr"), outputHandler.getLines());
@@ -73,7 +71,6 @@ public class CliProcessManagerTest {
     exception.expect(ExecutionException.class);
     exception.expectMessage("Process failed with exit code : 1");
     future.get();
-    Thread.sleep(1000); // wait for stderr is complete
 
     Assert.assertEquals(Collections.singletonList("stderr"), outputHandler.getLines());
   }
